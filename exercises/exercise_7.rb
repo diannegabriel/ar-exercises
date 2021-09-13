3d598ec0e121b6621d2ca362a4a8fe0db3007ac3
Dianne <mdianne.gabriel@gmail.com>
2021-09-13 18:35:39 -0400

    Complete exercise_7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,14 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+print "Store name: "
+@store_name = gets.chomp
+store = Store.create(name: @store_name)
+puts store.errors.messages
+
+bruh = Store.create(name: "Bruh", annual_revenue: -420, mens_apparel: false, womens_apparel: false)
+puts bruh.errors.messages
+
+employee = @store1.employees.create(first_name: "Cheese", last_name: "Curds", hourly_rate: 1000)
+puts employee.errors.messages 
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, presence: true, length: {minimum: 3}
+  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+
+  def apparel_authenticator
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "Stores must carry at least one of the men's or women's apparel")
+      errors.add(:womens_apparel, "Stores must carry at least one of the men's or women's apparel")
+    end
+  end
 end
